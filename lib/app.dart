a9eec60c2f3f13e3d194a64ebbc8a8ba4c9a541b
hydev777 <hytechwilson@gmail.com>
2024-04-07 11:27:42 -0400

    removed router folder

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../router/router.dart';
+import 'router.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
